4bc774a09616022609905227e4b5cb2d0f60e914
krishpranav <krisna.pranav@gmail.com>
2023-06-13 22:06:45 +0530

    java: ConfigUtil

--- src/main/java/org/example/utils/ConfigUtil.java
+++ src/main/java/org/example/utils/ConfigUtil.java
@@ -1,6 +1,7 @@
 package org.example.utils;
 
 import org.yaml.snakeyaml.Yaml;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,28 +10,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigUtil {
+
     private static String root = PackageListUtils.class.getClassLoader().getResource("").getPath();
-    private static String seperator = File.separator;
+    private static String separator = File.separator;
 
     public static Map<String, Object> getConfig() {
-        String filename = root + seperator + "jweb.yaml";
+        String filename = root + separator + "geisha.yaml";
         File file = new File(filename);
-        
         if (!file.exists()) {
             Map<String, Object> map = new HashMap<>();
             map.put("server", "jetty");
             map.put("port", 5200);
             return map;
         }
-
         try {
             InputStream input = new FileInputStream(file);
             Yaml yaml = new Yaml();
-            if (yaml == null) {
-                Map<String, Object> map = new HashMap<>();
+            Map<String, Object> map = (Map<String, Object>) yaml.load(input);
+            if (map == null) {
+                map = new HashMap<>();
             }
+            if (!map.containsKey("server")) {
+                map.put("server", "jetty");
+            }
+            if (!map.containsKey("port")) {
+                map.put("port", 5200);
+            }
+            return map;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
