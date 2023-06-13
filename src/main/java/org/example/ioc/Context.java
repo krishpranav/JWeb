80b39c75baeef99e713d581f4be9ce03d82a0013
krishpranav <krisna.pranav@gmail.com>
2023-06-13 20:20:02 +0530

    Context

---
+++ src/main/java/org/example/ioc/Context.java
@@ -0,0 +1,19 @@
+package org.example.ioc;
+
+import org.example.ioc.Bean;
+import org.example.ioc.Componenet;
+import org.example.ioc.Configuration;
+import org.example.ioc.Resource;
+
+
+import lombok.extern.slf4j.Slf4j;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+@Slf4j
+public class Context {
+    public static void init() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        
+    }
+}
