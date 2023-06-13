cd8d28adbbd52f5e5b62521cee2b0896742981ec
krishpranav <krisna.pranav@gmail.com>
2023-06-13 22:06:21 +0530

    java: ConfigUtil

--- src/main/java/org/example/utils/ConfigUtil.java
+++ src/main/java/org/example/utils/ConfigUtil.java
@@ -9,7 +9,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigUtil {
-    
-    
+    private static String root = PackageListUtils.class.getClassLoader().getResource("").getPath();
+    private static String seperator = File.separator;
 
+    public static Map<String, Object> getConfig() {
+        String filename = root + seperator + "jweb.yaml";
+        File file = new File(filename);
+        
+        if (!file.exists()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("server", "jetty");
+            map.put("port", 5200);
+            return map;
+        }
+
+        try {
+            InputStream input = new FileInputStream(file);
+            Yaml yaml = new Yaml();
+            if (yaml == null) {
+                Map<String, Object> map = new HashMap<>();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
