801fe504509732dbcb5e9244c4d472fe8da6c156
krishpranav <krisna.pranav@gmail.com>
2023-06-13 21:47:03 +0530

    java: Constants

---
+++ src/main/java/org/example/utils/Constants.java
@@ -0,0 +1,7 @@
+package org.example.utils;
+
+public class Constants {
+    public static final String DEFAULT_ENCODING = "UTF-8";  
+    public static final String CRLF = "\r\n";
+    public static final String CRLF_2 = "\r\n\r\n";
+}
